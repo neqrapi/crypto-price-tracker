ee7f862b78477a87e8421bee36ce39e9b3bb8bdf
neqrapi <60574161+neqrapi@users.noreply.github.com>
2021-08-12 21:43:59 +0200

    Add coin component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
   const [coins,setCoins]=useState([]);
+  const [search,setSearch]=useState('')
     useEffect(()=>{
       axios
       .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -13,10 +14,22 @@ function App() {
       console.log(res.data)
       }).catch(error =>console.log(error))
     })
+    const handleChange=e=>{
+      setSearch(e.target.value)
+    }
+    const filteredCoins=coins.filter(coin =>
+      coin.name.toLowerCase().includes(search.toLowerCase)
+
+    )
 
   return (
-    <div className="App">
-     
+    <div className="coin-app">
+     <div className="coin-search">
+       <h1 className="coin-text">Search a currency</h1>
+       <form>
+         <input type='text'placeholder="Search" className="coin-input" onChange={handleChange}></input>
+       </form>
+     </div>
     </div>
   );
 }
